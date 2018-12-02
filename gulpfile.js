0d8285ca8f3ede5a4ddf39cfb090ecba7c8e9fde
David <david_chou@ymail.com>
2018-12-02 18:05:45 -0500

    progress through 12

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,37 @@
+// "'old' version"
+
 const gulp = require("gulp"),
-      watch = require("gulp-watch")
+
+      watch = require("gulp-watch"),
+      postcss = require("gulp-postcss"),
+      autoprefixer = require("autoprefixer"),
+      cssvars = require("postcss-simple-vars"),
+      nested = require("postcss-nested")
 
 gulp.task("default", function() {
+
   console.log("Hooray -- you created a Gulp task (and in Git Bash's VI CLI text-editor, too!)!")
+
 })
 
 gulp.task("something_else", function() {
+
   console.log("Hooray -- another Gulp task created (also in VI as well!)!")
+
 })
 
 gulp.task("html", function() {
+
   console.log("Imagine something useful being done to the HTML here!")
+
 })
 
 gulp.task("css", function() {
-  console.log("Imagine Sass or PostCSS tasks running here!")
+
+  return gulp.src("./app/assets/styles/styles.css")
+  .pipe(postcss([autoprefixer, cssvars, nested]))
+  .pipe(gulp.dest("./app/temp/styles"))
+
 })
 
 gulp.task("watch", function() {
